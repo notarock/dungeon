e1776dae162264ba30825a0cdb9af18396d2c99f
Roch D'Amour <roch.damour@gmail.com>
2022-08-25 01:00:22 -0400

    Hallways

--- pkg/dungeon/generator.go
+++ pkg/dungeon/generator.go
@@ -19,6 +19,8 @@ func NewMap() *Map {
 	b.Partition(5, 75)
 	m.MakeRooms(b)
 
+	m.joinRooms(b)
+
 	sx, sy := b.GetStartingCoords()
 
 	m.StartX = sx
@@ -62,4 +64,46 @@ func (m *Map) MakeRoom(x1, x2, y1, y2 int) error {
 	m.Tiles = tiles
 
 	return nil
+}
+
+func (m *Map) joinRooms(bsp BspNode) {
+	if bsp.Front == nil || bsp.Back == nil {
+		return
+	}
+
+	sx := bsp.Front.minx + ((bsp.Front.maxx - bsp.Front.minx) / 2)
+	sy := bsp.Front.miny + ((bsp.Front.maxy - bsp.Front.miny) / 2)
+
+	dx := bsp.Back.minx + ((bsp.Back.maxx - bsp.Back.minx) / 2)
+	dy := bsp.Back.miny + ((bsp.Back.maxy - bsp.Back.miny) / 2)
+
+	switch {
+	case sx == dx:
+		if sy < dy {
+			for i := 0; i < (dy - sy); i++ {
+				m.Tiles[sx][sy+i] = tile.TileFloor{}
+			}
+		} else {
+			for i := 0; i < (sy - dy); i++ {
+				m.Tiles[sx][dy+i] = tile.TileFloor{}
+			}
+		}
+	case sy == dy:
+		if sy > dy {
+			for i := 0; i < (dx - sx); i++ {
+				m.Tiles[sx+i][sy] = tile.TileFloor{}
+			}
+		} else {
+			for i := 0; i < (sx - dx); i++ {
+				m.Tiles[dx+i][sy] = tile.TileFloor{}
+			}
+		}
+	}
+
+	// m.Tiles[sx][sy] = tile.TileWall{}
+	// m.Tiles[dx][dy] = tile.TileWall{}
+
+	m.joinRooms(*bsp.Front)
+	m.joinRooms(*bsp.Back)
+
 }
